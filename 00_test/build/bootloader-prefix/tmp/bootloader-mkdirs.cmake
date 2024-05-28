a61428243a0b9fdeea29b0b40b27b69a0a92847b
ajie <229546765@qq.com>
2024-05-28 17:37:49 +0800

    ESP32开发练习

---
+++ 00_test/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/Data/IoT/ESP32/esp/v5.2.1/esp-idf/components/bootloader/subproject"
+  "D:/Code/VSCode/ESP32/00_test/build/bootloader"
+  "D:/Code/VSCode/ESP32/00_test/build/bootloader-prefix"
+  "D:/Code/VSCode/ESP32/00_test/build/bootloader-prefix/tmp"
+  "D:/Code/VSCode/ESP32/00_test/build/bootloader-prefix/src/bootloader-stamp"
+  "D:/Code/VSCode/ESP32/00_test/build/bootloader-prefix/src"
+  "D:/Code/VSCode/ESP32/00_test/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/Code/VSCode/ESP32/00_test/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/Code/VSCode/ESP32/00_test/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ 00_test/main/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void app_main(void)
+{
+    printf("hello ajie!\n");
+    printf("ESP32demo");
+}
